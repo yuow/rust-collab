39b87debd43eadd842ecea90f9bb83b9b0694654
favorxo <favorxog@gmail.com>
2022-09-10 21:31:13 +0300

    feat: now gitignore should work

--- target/debug/deps/rust_collab-1503235033e61c97.d
+++
@@ -1,5 +0,0 @@
-/home/favor/code/rust-collab/target/debug/deps/rust_collab-1503235033e61c97.rmeta: src/main.rs
-
-/home/favor/code/rust-collab/target/debug/deps/rust_collab-1503235033e61c97.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/rust_collab-c558bba9b59e35bf.d
+++
@@ -1,5 +0,0 @@
-/home/favor/code/rust-collab/target/debug/deps/rust_collab-c558bba9b59e35bf.rmeta: src/main.rs
-
-/home/favor/code/rust-collab/target/debug/deps/rust_collab-c558bba9b59e35bf.d: src/main.rs
-
-src/main.rs:
