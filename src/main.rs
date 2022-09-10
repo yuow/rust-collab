675820057f4057c04be5530fc21dbdde7f1b63d2
ssushnost <ssushnost@gmail.com>
2022-09-11 00:50:13 +0300

    feat: make move feature

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::io;
-use std::io::*;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 enum Move {
     X,
     I,
@@ -11,23 +10,61 @@ enum Move {
 type Board = [[Move; 3]; 3];
 
 fn main() {
-    let mut board: Board = [[Move::I, Move::I, Move::I]; 3];
+    // TODO: win check func
+    let mut board: Board = [[Move::I; 3]; 3];
+    let mut current_move = Move::X;
     print_board(board);
-    get_input();
-    board[0][0] = Move::X;
-    board[0][1] = Move::O;
-}
-
-fn print_board(board: Board) {
-    for i in 0..3 {
-        println!("{:?}", board[i]);
+    loop {
+        let int_input = get_input();
+        board = make_move(current_move, board, int_input);
+        if current_move == Move::X {
+            current_move = Move::O;
+        } else if current_move == Move::O {
+            current_move = Move::X
+        }
+        print_board(board);
     }
 }
 
-fn get_input() {
+fn print_board(board: Board) {
+    for y in 0..3 {
+        for x in 0..3 {
+            if board[y][x] == Move::I {
+                print!("{} ", y * 3 + x + 1);
+            } else {
+                print!("{:?} ", board[y][x]);
+            }
+        }
+        println!();
+    }
+}
+
+fn get_input() -> usize {
     let mut input = String::new();
     io::stdin()
         .read_line(&mut input)
         .expect("error: unable to read user input");
-    println!("{}", input);
+    let int_input = input
+        .trim()
+        .parse::<usize>()
+        .expect("please give me corrent string number");
+    // crushes when get string input (ex. onetwothree)
+    return int_input;
+}
+
+fn make_move(current_move: Move, mut board: Board, mut input: usize) -> Board {
+    while !(1..10).contains(&input) {
+        println!("write number between 1 and 9");
+        input = get_input();
+    }
+    let mut x = (input - 1) % 3;
+    let mut y = (input - 1) / 3;
+    while board[y][x] != Move::I {
+        println!("cell filled");
+        input = get_input();
+        x = (input - 1) % 3;
+        y = (input - 1) / 3;
+    }
+    board[(input - 1) / 3][(input - 1) % 3] = current_move;
+    return board;
 }
