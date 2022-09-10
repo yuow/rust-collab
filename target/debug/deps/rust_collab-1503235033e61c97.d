35204591b37099ce666ad8f2d58f08773485a9bc
favorxo <favorxog@gmail.com>
2022-09-10 21:24:19 +0300

    feat: testing collab

---
+++ target/debug/deps/rust_collab-1503235033e61c97.d
@@ -0,0 +1,5 @@
+/home/favor/code/rust-collab/target/debug/deps/rust_collab-1503235033e61c97.rmeta: src/main.rs
+
+/home/favor/code/rust-collab/target/debug/deps/rust_collab-1503235033e61c97.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/rust_collab-c558bba9b59e35bf.d
@@ -0,0 +1,5 @@
+/home/favor/code/rust-collab/target/debug/deps/rust_collab-c558bba9b59e35bf.rmeta: src/main.rs
+
+/home/favor/code/rust-collab/target/debug/deps/rust_collab-c558bba9b59e35bf.d: src/main.rs
+
+src/main.rs:
